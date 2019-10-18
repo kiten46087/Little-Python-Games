807a809492ad5ee646e9122f73044a290c449b45
Qitian Lin <592692725@qq.com>
2019-10-17 21:55:39 -0700

    set up the game structure

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This is a repository of code of my python programming exercise.
 
 The exercise is about rewrite the little game projects from the book "Make games with Python and Pygame" written by AI Sweigart using python (espeically using pygame library).
+
+### Game1: Memory Puzzle
+
---
+++ index.py
@@ -0,0 +1,68 @@
+'''
+Memory Puzzle
+Qitian Lin
+'''
+import random, pygame, sys
+from pygame.locals import *
+
+# Set the over all structure.
+# Define the global variables that will be used.
+
+# Define the game board such as the width height of the game board.
+# and the weight and height of each icons.
+
+# Define the global variables colors that will be used.
+
+# Define the shape of the icon object such as square, diamond.
+
+# Draw icons method.
+def draw_icon():
+    pass
+
+# Draw Board method.
+def draw_board():
+    pass
+
+# Get the game board structure which return a 2D list that randomly generate the 
+# game board.
+def get_random_voard_strucutre():
+    pass
+
+# get raondomly the boxes inside the game board that are covered.
+def get_revearl_boxes():
+    pass
+
+# Show start game animation.
+def start_game_anime():
+    pass
+
+# Show end game animation.
+def end_game_anime():
+    pass
+
+# Show win the game animation.
+def win_game_anime():
+    pass
+
+# Box reveal animation.
+def box_reveal_animation():
+    pass
+
+# Get the mouse click event.
+def get_event():
+    pass
+
+# Handle the event that happened
+def handle_event():
+    pass
+
+# Initial the game setting
+def init_game():
+    pass
+
+# main game funciton.
+def main():
+    init_game()
+    get_event()
+    handle_event()
+    
